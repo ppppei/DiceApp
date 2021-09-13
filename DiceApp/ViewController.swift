03a2cd4533fa5ed63c40cda11e7149807f495a2b
ppppei <77483042+ppppei@users.noreply.github.com>
2021-09-13 14:48:06 -0700

    Initial Commit

---
+++ DiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiceApp
+//
+//  Created by PPEI on 9/13/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
