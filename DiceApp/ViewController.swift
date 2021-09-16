6c84b24464fa4d2f4f98f3c3f48c33f57ae5561c
ppppei <77483042+ppppei@users.noreply.github.com>
2021-09-16 11:12:05 -0700

    Committing updates

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -8,12 +8,78 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var imgOne: UIImageView!
+    
+    
+    @IBOutlet weak var imgTwo: UIImageView!
+    
+    @IBOutlet weak var lblScore: UILabel!
+    
+    var score = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func below7Pressed(_ sender: Any) {
+        let one = Int.random(in: 1...6)//define one
+        let two = Int.random(in: 1...6)//define two
+        
+        let imgOneName = "Dice\(one)"
+        let imgTwoName = "Dice\(two)"
+        //name of the image of random number
+        
+        imgOne.image = UIImage(named: imgOneName)
+        imgTwo.image = UIImage(named: imgTwoName)
+        
+        if one + two < 7{
+            score += 1
+        }else{
+            score -= 1
+        }
+        lblScore.text = "score = \(score)"
+    }
+    
+    @IBAction func lucky7Pressed(_ sender: Any) {
+        let one = Int.random(in: 1...6)//define one
+        let two = Int.random(in: 1...6)//define two
 
+        let imgOneName = "Dice\(one)"
+        let imgTwoName = "Dice\(two)"
+        //name of the image of random number
+
+        imgOne.image = UIImage(named: imgOneName)
+        imgTwo.image = UIImage(named: imgTwoName)
+
+        if one + two == 7{
+            score += 7
+        }else{
+            score -= 1
+        }
+        lblScore.text = "score = \(score)"
+    }
+    
+    @IBAction func above7Pressed(_ sender: Any) {
+        let one = Int.random(in: 1...6)//define one
+        let two = Int.random(in: 1...6)//define two
+
+        let imgOneName = "Dice\(one)"
+        let imgTwoName = "Dice\(two)"
+        //name of the image of random number
+
+        imgOne.image = UIImage(named: imgOneName)
+        imgTwo.image = UIImage(named: imgTwoName)
+        
+        if one + two > 7{
+            score += 1
+        }else{
+            score -= 1
+        }
+        lblScore.text = "score = \(score)"
+    }
+    
 }
 
